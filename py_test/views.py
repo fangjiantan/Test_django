89733a3c386d1df45626a53fbd2191df755fc0d2
FJT <fangjiantan@sina.cn>
2018-07-13 15:41:17 +0800

    1、实现登录功能，登录请求处理，登录成功页面

--- py_test/views.py
+++ py_test/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    #return HttpResponse("Hello FJT!")
+    return render(request,"index.html")
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if username == 'admin' and password == 'admin123':
+            return HttpResponse('login success!')
+        else:
+            return render(request,'index.html',{'error':'username or password error!'})
+
