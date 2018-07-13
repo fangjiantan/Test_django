5a68e62fcef46847f3b4adeb895ae151fa61117a
FJT <fangjiantan@sina.cn>
2018-07-13 18:09:58 +0800

    更新20180713，新增cookie功能

--- py_test/views.py
+++ py_test/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+
 # Create your views here.
 def index(request):
     #return HttpResponse("Hello FJT!")
@@ -13,7 +15,12 @@ def login_action(request):
         username = request.POST.get('username','')
         password = request.POST.get('password','')
         if username == 'admin' and password == 'admin123':
-            return HttpResponse('login success!')
+            response = HttpResponseRedirect('/event_manage/')
+            response.set_cookie('user',username,3600)
+            return response
         else:
             return render(request,'index.html',{'error':'username or password error!'})
 
+def event_manage(request):
+    username = request.COOKIES.get('user','')
+    return render(request,"event_manage.html",{"user":username})
